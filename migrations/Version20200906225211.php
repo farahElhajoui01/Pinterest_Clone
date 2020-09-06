ed5ca6f094824d8a00f246b0d718e17c745d08ff
farah <farahelhajoui01@gmail.com>
2020-09-07 00:01:23 +0100

    add createdAt and updatedAt ,sorted list

--- migrations/Version20200906225211.php
+++ migrations/Version20200906225211.php
@@ -10,22 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200906205748 extends AbstractMigration
+final class Version20200906225211 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return 'Create pin table';
+        return '';
     }
 
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE pin (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE pin ADD created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, ADD updated_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE pin');
+        $this->addSql('ALTER TABLE pin DROP created_at, DROP updated_at');
     }
 }
--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -5,16 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PinRepository;
+use App\Entity\Pin;
+
 
 class PinController extends AbstractController
 {
     /**
-     * @Route("/pin", name="app_home")
+     * @Route("/pins", name="app_home")
      */
     public function index(PinRepository $pinRepository )
     
     {
-        $pins=$pinRepository->findAll();
+        $pins=$pinRepository->findBy([],['createdAt'=>'DESC']);
         return $this->render('pin/index.html.twig', compact('pins'));
+    }
+
+    
+    /**
+     * @Route("/pins/{id<[0-9]>}", name="app_pin_show")
+     */
+    public function show(Pin $pin )
+    
+    {
+   return $this->render('pin/show.html.twig', compact('pin'));
     }
 }
